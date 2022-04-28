8a80f73af743448ffac5a614679f8e9fe6564001
7Ji <pugokushin@gmail.com>
2022-04-28 20:35:02 +0800

    mibox3/filesystem: (enable_usb_boot.sh) fix default value for upgrade_step 2->0, fix subshell redirect

--- projects/Amlogic/devices/mibox3/filesystem/usr/sbin/enable_usb_boot.sh
+++ projects/Amlogic/devices/mibox3/filesystem/usr/sbin/enable_usb_boot.sh
@@ -9,7 +9,7 @@ function safe_set_env() {
     local ENV_KEY="$1"
     local ENV_VALUE="$2"
     local ENV_DEFAULT="$3"
-    local ENV_EXIST=$($PRINTENV -n  "$1") 2>/dev/null
+    local ENV_EXIST=$($PRINTENV -n  "$1" 2>/dev/null)
     if [[ $? == 0 ]]; then
         if [[ "$ENV_EXIST" == "$ENV_VALUE" ]]; then
             echo "Note: u-boot environment '$ENV_KEY' is already set, no need to update"
@@ -27,7 +27,7 @@ function safe_set_env() {
 safe_set_env storeboot 'run storeargs; ${bootcmd}'
 safe_set_env dtb_mem_addr 1000000
 safe_set_env bootfromnand 0
-safe_set_env upgrade_step 2 2
+safe_set_env upgrade_step 2 0
 safe_set_env usbdtb 'if fatload usb 0 ${dtb_mem_addr} dtb.img; then else store dtb read $dtb_mem_addr; fi'
 safe_set_env bootfromusb 'usb start 0; if fatload usb 0 ${loadaddr} kernel.img; then run usbdtb; setenv bootargs ${bootargs} bootfromusb; bootm; fi'
 safe_set_env bootcmd 'if test ${bootfromnand} = 1; then setenv bootfromnand 0; saveenv; else run bootfromusb; fi; run storeboot' 'run boot_from_flash'
