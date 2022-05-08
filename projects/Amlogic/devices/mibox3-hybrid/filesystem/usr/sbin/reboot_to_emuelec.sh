becafc4b24fa0b212c913ea95f609f6350d2e476
7Ji <pugokushin@gmail.com>
2022-05-08 09:09:13 +0800

    mibox3-hybrid/reboot_to_emuelec: update partition name

--- projects/Amlogic/devices/mibox3-hybrid/filesystem/usr/sbin/reboot_to_emuelec.sh
+++ projects/Amlogic/devices/mibox3-hybrid/filesystem/usr/sbin/reboot_to_emuelec.sh
@@ -20,7 +20,7 @@ else
     $SETENV upgrade_step 2
     $SETENV usbdtb 'if fatload usb 0 ${dtb_mem_addr} dtb.img; then else store dtb read ${dtb_mem_addr}; fi'
     $SETENV boot_from_usb 'usb start 0; if fatload usb 0 ${loadaddr} kernel.img; then run usbdtb; setenv bootargs ${bootargs} bootfromusb; bootm; fi'
-    $SETENV boot_from_emmc 'if itest ${bootemuelec} == 1; then imgread kernel recovery ${loadaddr}; else imgread kernel boot ${loadaddr}; fi; bootm'
+    $SETENV boot_from_emmc 'if itest ${bootemuelec} == 1; then imgread kernel eboot ${loadaddr}; else imgread kernel boot ${loadaddr}; fi; bootm'
     $SETENV bootcmd 'if itest ${bootfromnand} == 1; then setenv bootfromnand 0; saveenv; else run boot_from_usb; fi; run boot_from_emmc; run storeboot'
 fi
 
